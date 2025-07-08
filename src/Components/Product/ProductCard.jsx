7af78427e3f35200386eb7fad5d5de598a6ede6d
yemisrach-19 <yemi.t.maki@gmail.com>
2025-07-07 17:33:34 -0800

    description

--- src/Components/Product/ProductCard.jsx
+++ src/Components/Product/ProductCard.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import Rating from "@mui/material/Rating";
 import CurrencyFormatter from "../CurrencyFormatter/CurrencyFormatter";
 import classes from "./Product.module.css";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-function ProductCard({ product }) {
-  const { id, image, title, rating, price  } = product;
+function ProductCard({ product, flex, renderDesc }) {
+  const { id, image, title, rating, price, description } = product;
   return (
-    <div className={`${classes.card__container}`}>
+    <div
+      className={`${classes.card__container} ${
+        flex ? classes.product__flexed : ""
+      }`}
+    >
       <Link to={`/products/${id}`}>
-        <img src={image} alt=""className={classes.img_container} />
+        <img src={image} alt="" className={classes.img_container} />
       </Link>
       <div>
         <h3>{title}</h3>
+        {renderDesc && <div style={{ maxWidth: "700px" }}>{description}</div>}
         <div className={classes.rating}>
           <Rating value={rating?.rate} precision={0.1} />
           <small>{rating?.count}</small>
--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -28,7 +28,16 @@ const ProductDetail = () => {
 
   return (
     <LayOut>
-      {isLoading ? <Loader /> : <ProductCard product={product} />}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ProductCard
+          product={product}
+          flex={true}
+          renderDesc={true}
+        
+        />
+      )}
     </LayOut>
   );
 };
